891a530b8dad64053cb93545af4d5b5bc3afcf64
Felix Bade <flix.bade@gmail.com>
2020-12-05 12:14:51 +0200

    Remove visit count functionality

--- treelist_single_user_server.py
+++ treelist_single_user_server.py
@@ -4,25 +4,17 @@ import shelve
 from flask import Flask
 from flask import request, Response
 
-from secret import secret_path
+from secret import secret_path, port
 
 app = Flask(__name__)
 db = shelve.open('treelist.data')
-
-if 'visit-count' not in db:
-    db['visit-count'] = 0
 
 def json_response(obj):
     return Response(json.dumps(obj), mimetype='application/json')
 
 @app.route('/')
 def index():
-    #resp = request.headers.environ['HTTP_X_REAL_IP']
-    db['visit-count'] += 1
-
-    resp = {'status': 'ok', 'visit-count': db['visit-count']}
-
-    return json_response(resp)
+    return json_response({'status': 'ok'})
 
 @app.route('/' + secret_path, methods=['POST'])
 def write():
@@ -36,5 +28,5 @@ def read():
     data = db.get('data', {})
     return json_response(data)
 
-app.run(port=3000, debug=False)
+app.run(port=port, debug=False)
 db.close()
