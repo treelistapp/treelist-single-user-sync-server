ff029ff41dbf90e1c49ed6419c22aafc4d4ba061
Felix Bade <flix.bade@gmail.com>
2021-02-06 18:18:08 +0200

    Add data hash and route for it

--- treelist_single_user_server.py
+++ treelist_single_user_server.py
@@ -1,5 +1,6 @@
 import json
 import shelve
+import random
 
 from flask import Flask
 from flask import request, Response
@@ -18,15 +19,26 @@ def json_response(obj):
 def index():
     return json_response({'status': 'ok'})
 
+@app.route(secret_path + '/hash')
+def get_hash():
+    hash_value = db.get('hash', 0)
+    return json_response({'hash': hash_value})
+
 @app.route(secret_path, methods=['POST'])
 def write():
     data = request.json
     db['data'] = data
-    return json_response({'status': 'ok'})
+    hash_value = random.random() # good enough because no false negatives and <1% false positives in normal use
+    db['hash'] = hash_value
+    return json_response({
+        'status': 'ok',
+        'hash': hash_value
+    })
 
 @app.route(secret_path, methods=['GET'])
 def read():
     data = db.get('data', {})
+    data['hash'] = db.get('hash', 0)
     return json_response(data)
 
 app.run(port=port, debug=False)
